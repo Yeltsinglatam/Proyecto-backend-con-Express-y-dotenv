e605685038e9369cea6ad747a9bc960d4052adcb
YELTSIN <yeltsing57@gmail.com>
2025-10-05 22:26:13 -0300

    nd-con-Express-y-doten

--- myApp.js
+++ myApp.js
@@ -1,40 +1,52 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// simple logger
 app.use(function (req, res, next) {
-    console.log(`${req.method} ${req.path} - ${req.ip}`);
-    next();
-})
-
-app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
-
-app.use("/public", express.static(__dirname + "/public"));
-
-app.get("/json", (req, res) => {
-    const json = { message: "Hello json" };
-    json.message = process.env.MESSAGE_STYLE === "uppercase" ? json.message.toUpperCase() : json.message;
-    res.json(json);
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
 });
 
-app.get("/now", (req, res, next) => {
-        req.time = new Date().toString();
-        next();
-    },
-    (req, res) => res.json({ time: req.time })
+// root
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
+
+// static assets
+app.use('/public', express.static(__dirname + '/public'));
+
+// >>> /json (LEE process.env DENTRO DEL HANDLER) <<<
+app.get('/json', (req, res) => {
+  const base = 'Hello json';
+  const message =
+    process.env.MESSAGE_STYLE === 'uppercase' ? base.toUpperCase() : base;
+  res.json({ message });
+});
+
+// chained middleware example
+app.get(
+  '/now',
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => res.json({ time: req.time })
 );
 
-app.get("/:word/echo", (req, res) => res.json({ echo: req.params.word }));
+// echo
+app.get('/:word/echo', (req, res) => res.json({ echo: req.params.word }));
 
-app.route("/name")
-    .get((req, res) => {
-        res.json({ name: `${req.query.first} ${req.query.last}` });
-    })
-    .post((req, res) => {
-        res.json({ name: `${req.body.first} ${req.body.last}` });
-    })
+// query + body
+app
+  .route('/name')
+  .get((req, res) => {
+    res.json({ name: `${req.query.first} ${req.query.last}` });
+  })
+  .post((req, res) => {
+    res.json({ name: `${req.body.first} ${req.body.last}` });
+  });
 
 module.exports = app;
